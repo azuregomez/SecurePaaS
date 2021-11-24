78b61ae2047d7e9ed0e7e9ac623f901e5e78e447
Eduardo Gomez <edgome@microsoft.com>
2021-11-24 15:51:36 -0600

    Update azuredeploy.ps1

--- azuredeploy.ps1
+++ azuredeploy.ps1
@@ -7,7 +7,7 @@ Param(
 $templateFilePath = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $TemplateFile))
 $templateParametersFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $ParameterFile))
 $params = get-content $templateParametersFile | ConvertFrom-Json
-$prefix = $params.parameters.resourcenameprefix.value
+$prefix = $params.parameters.projectname.value
 $rgname = $prefix + "-rg"
 # Create the resource group only when it doesn't already exist
 if ($null -eq (Get-AzResourceGroup -Name $rgname -Location $Location -Verbose -ErrorAction SilentlyContinue)) {
